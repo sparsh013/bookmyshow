43aeb40d1a46f9d54dea883d8a58c0fa9df60913
sparsh013 <oltikarsparsh013@gmail.com>
2024-11-14 14:30:58 +0530

    Update  commit

--- back-end/schema.js
+++ back-end/schema.js
@@ -4,7 +4,7 @@ const TicketSchema = new mongoose.Schema({
   movie: { type: String, required: true },
   slot: { type: String, required: true },
   seats: {
-    A1: { type: Number, default: 0 }, // 0 means available, 1 means booked
+    A1: { type: Number, default: 0 }, 
     A2: { type: Number, default: 0 },
     A3: { type: Number, default: 0 },
     A4: { type: Number, default: 0 },
